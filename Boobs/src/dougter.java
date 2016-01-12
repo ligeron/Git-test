3d6b4e8df383c4c419b247d5075660b58e34652d
Pavlo Poltroratskyi <ligeron.4g@gmail.com>
2016-01-12 16:43:50 +0200

    add 1 change

--- Boobs/src/dougter.java
+++ Boobs/src/dougter.java
@@ -4,7 +4,8 @@ public class dougter extends Mother {
 	public String dougterMethod(String n){
 		
 		String str = "But I can overight Mother.java method";
-		return str = str+n;
+		return 
+				str = str+n;
 		
 	}
 
